25bfddee1eee93979a07789cea22092440ef8c02
donvalent <Donvalent@yandex.ru>
2020-07-29 14:54:00 +0300

    Push on github

--- src/MyENV.php
+++ src/MyENV.php
@@ -4,19 +4,10 @@ namespace MyENV;
 
 require '../vendor/autoload.php';
 
-namespace MyENV;
+use Dotenv\Dotenv;
 
 class MyENV
 {
-    /*
-     * Writing env on home project directory
-     */
-    public function __construct()
-    {
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-    }
-
     /**
      * Get
      *
@@ -25,6 +16,8 @@ class MyENV
      */
     public static function get(string $env)
     {
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
         return array_key_exists($env, $_ENV) ? $_ENV[$env] : 'ERROR : Env param is missing';
     }
 }
